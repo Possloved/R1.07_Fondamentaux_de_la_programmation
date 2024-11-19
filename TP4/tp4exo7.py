250a340178b82fb56f3db32af4a904f6aa5040e8
possloved <axel.papillier@uha.fr>
2024-11-19 16:29:42 +0100

    f

--- TP4/tp4exo7.py
+++ TP4/tp4exo7.py
@@ -1,9 +1,10 @@
-x = None
-y = None
-binome = ()
+binome = ('login1', 'login2')
 
-x=input("enter name1 :")
-y = list(binome)
-y.append(x)
-binome = tuple(y)
-print(binome)
+print(f"L'étudiant {binome[0]} est en binôme avec l'étudiant {binome[1]}")
+
+# DEL only one '#' in one of the two following lines, it's to display what type of error will happen.
+# binome[1] = 'nouveau_login'
+# del binome[1]
+
+binome = binome + ('login3',)
+print(f"Le trinôme est composé de {binome}")
---
+++ TP4/tp4exo8.py
@@ -0,0 +1,27 @@
+infoEtudiant = {'firstname': 'Walter', 'name': 'White','promo': '2008','group': 'Breaking Bad'}
+print(f"Votre prénom est '{infoEtudiant['firstname']}', votre nom est '{infoEtudiant['name']}', vous faites partie de la promo '{infoEtudiant['promo']}' et votre groupe est '{infoEtudiant['group']}'")
+
+print("\nLes clés du dictionnaire sont :")
+for key in infoEtudiant.keys():
+    print(f"- {key}")
+
+print("\nLes valeurs du dictionnaire sont :")
+for value in infoEtudiant.values():
+    print(f"- {value}")
+
+print("\nLes tuplets du dictionnaire sont :")
+for item in infoEtudiant.items():
+    print(f"- {item}")
+
+infoVoisin = {'firstname': 'Bruce','name': 'Wayne','promo': '2005','group': 'Batman Begins'}
+print(f"\nL'autre étudiant a pour prénom '{infoVoisin['firstname']}', nom '{infoVoisin['name']}', promo '{infoVoisin['promo']}' et groupe '{infoVoisin['group']}'")
+
+binome = {'id1': infoEtudiant,'id2': infoVoisin}
+
+print("\nDictionnaire binôme :")
+for idEtudiant, info in binome.items():
+    print(f"ID : {idEtudiant}, Info : {info}")
+
+print("\nLes étudiants formant le binôme sont :")
+for idEtudiant, info in binome.items():
+    print(f"- L'étudiant {info['firstname']} {info['name']} du groupe {info['group']}")
