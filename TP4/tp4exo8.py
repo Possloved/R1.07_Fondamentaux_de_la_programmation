0723826801fbd7e6015b05b0f26bf09f77378f96
possloved <axel.papillier@uha.fr>
2024-11-19 16:30:48 +0100

    f

--- TP4/tp4exo8.py
+++ TP4/tp4exo8.py
@@ -4,11 +4,9 @@ print(f"Votre prénom est '{infoEtudiant['firstname']}', votre nom est '{infoEtu
 print("\nLes clés du dictionnaire sont :")
 for key in infoEtudiant.keys():
     print(f"- {key}")
-
 print("\nLes valeurs du dictionnaire sont :")
 for value in infoEtudiant.values():
     print(f"- {value}")
-
 print("\nLes tuplets du dictionnaire sont :")
 for item in infoEtudiant.items():
     print(f"- {item}")
@@ -17,11 +15,9 @@ infoVoisin = {'firstname': 'Bruce','name': 'Wayne','promo': '2005','group': 'Bat
 print(f"\nL'autre étudiant a pour prénom '{infoVoisin['firstname']}', nom '{infoVoisin['name']}', promo '{infoVoisin['promo']}' et groupe '{infoVoisin['group']}'")
 
 binome = {'id1': infoEtudiant,'id2': infoVoisin}
-
 print("\nDictionnaire binôme :")
 for idEtudiant, info in binome.items():
     print(f"ID : {idEtudiant}, Info : {info}")
-
 print("\nLes étudiants formant le binôme sont :")
 for idEtudiant, info in binome.items():
     print(f"- L'étudiant {info['firstname']} {info['name']} du groupe {info['group']}")
